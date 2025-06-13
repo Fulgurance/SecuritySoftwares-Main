c2feb48ef9f70b33f128ff90f6c13d485228ca11
Fulgurance <zohran.londais@gmail.com>
2025-06-13 12:34:42 +0100

    Improved nessus installer

--- Nessus/10.8.4/10.8.4.cr
+++ Nessus/10.8.4/10.8.4.cr
@@ -1,2 +1,22 @@
 class Target < ISM::PackagedSoftware
+
+    def prepareInstallation
+        if option("Openrc")
+            prepareOpenrcServiceInstallation(   path:   "#{workDirectoryPath}/Nessusd-Init.d",
+                                                name:   "nessusd")
+        end
+
+        super
+    end
+
+    def deploy
+        super
+
+        if autoDeployServices
+            if option("Openrc")
+                runRcUpdateCommand("add nessusd default")
+            end
+        end
+    end
+
 end
